e32e07ea132b1e334a50a995e9764793eb243934
ViniciusW <dev9@aproms.com.br>
2026-02-02 17:00:45 -0400

    Add GSAP dependency and refactor PomodoroTimer to implement mode handling with local storage

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -9,10 +9,51 @@ import {
 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
-const PomodoroTimer = () => {
-  const [currentMode, setCurrentMode] = useState(`focus`);
+const MODES = {
+  focus: {
+    label: `Foco`,
+    time: 1500,
+    icon: <Brain />,
+    card: 'bg-rose-500 text-white shadow-lg shadow-rose-500/30',
+    hover: 'hover:text-rose-500 ',
+    primaryColor: 'text-rose-500',
+    backgroundColor: 'bg-rose-500',
+    shadowColor: 'shadow-rose-500/30',
+  },
+  short: {
+    label: `Curta`,
+    time: 300,
+    icon: <Coffee />,
+    card: '  bg-emerald-500 text-white shadow-lg shadow-emerald-500/30',
+    hover: 'hover:text-emerald-500 ',
+    primaryColor: 'text-emerald-500',
+    backgroundColor: 'bg-emerald-500',
+    shadowColor: 'shadow-emerald-500/30',
+  },
+  long: {
+    label: `Longa`,
+    time: 900,
+    icon: <Armchair />,
+    card: 'bg-blue-500 text-white shadow-lg shadow-blue-500/30',
+    hover: 'hover:text-blue-500',
+    primaryColor: 'text-blue-500',
+    backgroundColor: 'bg-blue-500',
+    shadowColor: 'shadow-blue-500/30',
+  },
+};
 
-  const [secondsTotal, setSecondsTotal] = useState(1500);
+const PomodoroTimer = () => {
+  const pomodoroInicio = localStorage.getItem('pomodoro_seconds');
+
+  const pomodoroMode = localStorage.getItem('pomodoro_mode');
+
+  const [currentMode, setCurrentMode] = useState(() => {
+    return pomodoroMode === undefined || null ? 'focus' : pomodoroMode;
+  });
+
+  const [secondsTotal, setSecondsTotal] = useState(() => {
+    return pomodoroInicio === undefined || null ? 1500 : Number(pomodoroInicio);
+  });
   const [isActive, setIsActive] = useState(false);
   const formatTime = (totalSeconds) => {
     const minutes = Math.floor(totalSeconds / 60);
@@ -20,6 +61,31 @@ const PomodoroTimer = () => {
     return `${minutes}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
   };
   formatTime(secondsTotal);
+
+  localStorage.setItem('pomodoro_mode', currentMode);
+
+  useEffect(() => {
+    localStorage.setItem('pomodoro_seconds', secondsTotal.toString());
+  }, [secondsTotal]);
+
+  useEffect(() => {
+    localStorage.setItem('pomodoro_mode', currentMode);
+  }, [currentMode]);
+
+  useEffect(() => {
+    const body = document.body;
+    const modeColors = {
+      focus: '#fff1f2',
+      short: '#ECFDF5',
+      long: 'rgb(239, 246, 255)',
+    };
+
+    body.style.backgroundColor = modeColors[currentMode];
+
+    return () => {
+      body.style.backgroundColor = '';
+    };
+  }, [currentMode]);
 
   useEffect(() => {
     if (!isActive || secondsTotal <= 0) return;
@@ -30,38 +96,10 @@ const PomodoroTimer = () => {
     return () => clearInterval(interval);
   }, [isActive, secondsTotal]);
 
-  const MODES = {
-    focus: {
-      label: `Foco`,
-      time: 1500,
-      icon: <Brain />,
-      card: 'bg-rose-500 text-white shadow-lg shadow-rose-500/30',
-      hover: 'hover:text-rose-500 ',
-      primaryColor: 'text-rose-500',
-      backgroundColor: 'bg-rose-500',
-      shadowColor: 'shadow-rose-500/30',
-    },
-    short: {
-      label: `Curta`,
-      time: 300,
-      icon: <Coffee />,
-      card: '  bg-emerald-500 text-white shadow-lg shadow-emerald-500/30',
-      hover: 'hover:text-emerald-500 ',
-      primaryColor: 'text-emerald-500',
-      backgroundColor: 'bg-emerald-500',
-      shadowColor: 'shadow-emerald-500/30',
-    },
-    long: {
-      label: `Longa`,
-      time: 900,
-      icon: <Armchair />,
-      card: 'bg-blue-500 text-white shadow-lg shadow-blue-500/30',
-      hover: 'hover:text-blue-500',
-      primaryColor: 'text-blue-500',
-      backgroundColor: 'bg-blue-500',
-      shadowColor: 'shadow-blue-500/30',
-    },
-  };
+  function resetTime() {
+    setSecondsTotal(MODES[currentMode].time);
+    setIsActive(false);
+  }
 
   function handleChangeMode(modeKey) {
     setCurrentMode(modeKey);
@@ -78,9 +116,9 @@ const PomodoroTimer = () => {
             onClick={() => {
               handleChangeMode(modeKey);
             }}
+            key={modeKey}
           >
             <button
-              key={modeKey}
               className={`px-8.5 py-2 text-gray-600 rounded-full text-sm font-medium transition-all duration-300 flex items-center gap-2    ${currentMode === modeKey ? MODES[modeKey].card : MODES[modeKey].hover}`}
             >
               {MODES[modeKey].icon}
@@ -92,11 +130,11 @@ const PomodoroTimer = () => {
 
       <div>
         <p
-          className={`${MODES[currentMode].primaryColor} text-[8rem] leading-none font-bold tracking-tighter tabular-nums transition-colors duration-300 select-none`}
+          className={`${MODES[currentMode].primaryColor} text-[8rem] leading-none font-bold tracking-tighter tabular-nums transition-colors duration-300 select-none `}
         >
           {formatTime(secondsTotal)}
         </p>
-        <p className="bottom-6 w-full text-center text-sm font-medium opacity-50 uppercase tracking-widest">
+        <p className="bottom-6 w-full text-center text-sm font-medium opacity-50 uppercase tracking-widest mb-3.5">
           {isActive ? 'EM PROGRESSO' : 'PAUSADO'}
         </p>
       </div>
@@ -114,6 +152,7 @@ const PomodoroTimer = () => {
           )}
         </button>
         <button
+          onClick={resetTime}
           className="w-14 h-14 rounded-2xl flex items-center justify-center transition-all border-2
               border-black/5 hover:bg-black/5
             "
